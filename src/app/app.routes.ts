eac1921e01850b5d94cf242bd8b7d866e9e402fe
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-07 17:02:01 +0100

    componentes alumnos, cursos, examenes y navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'alumnos', 
+        loadComponent: () => import('./components/alumnos/alumnos.component')
+        .then((c) => c.AlumnosComponent)
+    },
+    {
+        path: 'cursos', 
+        loadComponent: () => import('./components/cursos/cursos.component')
+        .then((c) => c.CursosComponent)
+    },
+    {
+        path: 'examenes', 
+        loadComponent: () => import('./components/examenes/examenes.component')
+        .then((c) => c.ExamenesComponent)
+    }
+];
---
+++ src/app/components/examenes/examenes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-examenes',
+  standalone: true,
+  imports: [],
+  templateUrl: './examenes.component.html',
+  styleUrl: './examenes.component.scss'
+})
+export class ExamenesComponent {
+
+}
