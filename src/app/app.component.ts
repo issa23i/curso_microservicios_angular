90fc739ba31a14740e4e99ae0823840d4a0230ce
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-08 11:43:22 +0100

    rutas lazy load en standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgOptimizedImage],
+  imports: [RouterOutlet,NgOptimizedImage, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
